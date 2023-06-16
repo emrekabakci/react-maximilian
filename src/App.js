81f42e1fc9c4c2734c00b735c799efa61fd1f9dc
emrekabakci <221238040@ogrenci.karatay.edu.tr>
2023-06-16 16:47:34 +0300

    Expense card moved in popup.

--- src/App.js
+++ src/App.js
@@ -2,43 +2,9 @@ import React, { useState } from "react";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpenses/NewExpense";
 import "./App.css";
-
-const DUMMY_EXPENSES = [
-  {
-    id: "e1",
-    title: "Toilet Paper",
-    amount: 94.12,
-    date: new Date(2020, 7, 14),
-  },
-  {
-    id: "e2",
-    title: "New TV",
-    amount: 799.49,
-    date: new Date(2021, 2, 12),
-  },
-  {
-    id: "e3",
-    title: "Car Insurance",
-    amount: 294.67,
-    date: new Date(2021, 2, 28),
-  },
-  {
-    id: "e4",
-    title: "New Desk (Wooden)",
-    amount: 450,
-    date: new Date(2021, 5, 12),
-  },
-];
+import ExpensesPopup from "./components/NewExpenses/ExpensesPopup";
 
 function App() {
-  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
-
-  const addExpenseHandler = (expense) => {
-    setExpenses((prevExpenses) => {
-      return [expense, ...prevExpenses];
-    });
-  };
-
   //  return React.createElement(
   //    "div",
   //    {},
@@ -48,8 +14,7 @@ function App() {
 
   return (
     <div>
-      <NewExpense onAddExpense={addExpenseHandler} />
-      <Expenses items={expenses} />
+      <ExpensesPopup></ExpensesPopup>
     </div>
   );
 }
